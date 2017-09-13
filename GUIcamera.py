6ec2b99ea8eaad9602a81ef4f101dbdb2f151843
imafikus <aleksatesicteske@gmail.com>
2017-09-13 02:37:05 +0200

    added are you sure

--- GUIcamera.py
+++ GUIcamera.py
@@ -1,14 +1,40 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
+from PyQt5.QtGui import QIcon
 
+
+class Example(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):
+        
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('PMG.png'))        
+    
+        self.show()
+
+    """
+        def closeEvent(self, event):
+        
+        reply = QMessageBox.question(self, 'Message',
+            "Are you sure to quit?", QMessageBox.Yes | 
+            QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+        """       
+        
+        
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('GUI')
-    w.show()
-    
+    ex = Example()
     sys.exit(app.exec_())
